e236181c4e9607bcb5ddc72cadfb81f5963c2a76
valenDanDev <valenciadaniel8022015@gmail.com>
2023-07-22 14:48:54 -0500

    order items to check out proccess

--- app/controllers/shipping_controller.rb
+++ app/controllers/shipping_controller.rb
@@ -1,18 +1,21 @@
 class ShippingController < ApplicationController
   def new
+    @order = Order.find(params[:order_id])
+    @order_items = @order.order_items
     @shipping = Shipping.new
   end
 
   def create
-    puts"hola probando el metodo"
-    puts"hola, se esta ensayando el metodo haber si funciona"
-    puts"hola probando el #{shipping_params}"
     @shipping = Shipping.new(shipping_params)
 
     if @shipping.save
+      # Update the associated order status to "completed"
+      order = Order.find_by(id: params[:order_id])
+      order.update(status: "completed") if order
+
       redirect_to success_shipping_path(order_id: @shipping.order_id)
     else
-      puts"error"
+      puts "error"
     end
   end
 
